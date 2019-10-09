a39faf9475b168a8e48e597a1024908ad47160e3
Alex Prascak <aprascak@ufl.edu>
2019-10-09 11:50:55 -0400

    Progress made on Statistics page.

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,11 +5,27 @@
 </template>
 
 <script>
+import ApexCharts from 'apexcharts'
+
 export default {
     name: 'Statistics',
     data() {
         return {
             user: null
+        }
+    },
+    mounted() {
+        var options = {
+            chart: {
+                type: 'Line'
+            },
+            series: [{
+                name: 'sales',
+                data: [30,40,35,50,49,60,70,91,125]
+            }],
+            xaxis: {
+                categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+            }
         }
     }
 }
